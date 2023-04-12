7d7b7d5f25d78768cc4fa3bab601685887cb521c
Eugene Du <eugened186@gmail.com>
2023-04-12 20:14:40 +0000

    address feedback

--- add_markers.sh
+++ add_markers.sh
@@ -5,4 +5,6 @@ xterm  -e  " source devel/setup.bash; roslaunch my_robot amcl_demo.launch" &
 sleep 5
 xterm  -e  " source devel/setup.bash; roslaunch my_robot home_service_view.launch" &
 sleep 5
-xterm  -e  "source devel/setup.bash; rosrun add_markers basic_shapes"
+xterm  -e  "source devel/setup.bash; rosrun add_markers basic_shapes" &
+sleep 5
+xterm  -e  "source devel/setup.bash; rosrun add_markers test_markers"
--- src/add_markers/src/add_markers_test.cpp
+++ src/add_markers/src/add_markers_test.cpp
@@ -1,25 +1,15 @@
 #include <ros/ros.h>
-#include <move_base_msgs/MoveBaseAction.h>
 #include <move_base_msgs/MoveBaseGoal.h>
 #include "std_msgs/Bool.h"
-#include <actionlib/client/simple_action_client.h>
  
-// Define a client for to send goal requests to the move_base server through a SimpleActionClient
-typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
 int main(int argc, char** argv){
   // Initialize the simple_navigation_goals node
-  ros::init(argc, argv, "simple_navigation_goals");
+  ros::init(argc, argv, "send_navigation_goals");
   ros::NodeHandle n;
   ros::Publisher pub = n.advertise<std_msgs::Bool>("/simple_navigation_goals/show", 1);
   ros::Publisher pose_pub = n.advertise<move_base_msgs::MoveBaseGoal>("/simple_navigation_goals/goal", 1);
-  //tell the action client that we want to spin a thread by default
-  MoveBaseClient ac("move_base", true);
 
-  // Wait 5 sec for move_base action server to come up
-  while(!ac.waitForServer(ros::Duration(5.0))){
-    ROS_INFO("Waiting for the move_base action server to come up");
-  }
 
   move_base_msgs::MoveBaseGoal goal;
 
@@ -36,28 +26,18 @@ int main(int argc, char** argv){
   n.getParam("/pickup_w", number_to_get);
   goal.target_pose.pose.orientation.w = number_to_get;
   
+  ros::Duration(1.0).sleep();
   std_msgs::Bool show;
   show.data = true;
    // Send the goal position and orientation for the robot to reach
   ROS_INFO("Sending pickup location [%f. %f]", goal.target_pose.pose.position.x, goal.target_pose.pose.position.y);
-  pub.publish(show);
   pose_pub.publish(goal);
-  ac.sendGoal(goal);
-  
-  // Wait an infinite time for the results
-  ac.waitForResult();
-  
-  // Check if the robot reached its goal{
-  if (ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
-    ROS_INFO("Hooray, the base moved to pickup location");
-    show.data = false;
-    pub.publish(show);
-	} else {
-    ROS_INFO("The base failed to move forward 1 meter for some reason");
-  }
-  
+  pub.publish(show);
   ros::Duration(5.0).sleep();
-
+  
+  show.data = false;
+  pub.publish(show);
+  ros::Duration(5.0).sleep();
   
   goal.target_pose.header.stamp = ros::Time::now();
    // Define a position and orientation for the robot to reach
@@ -71,18 +51,10 @@ int main(int argc, char** argv){
    // Send the goal position and orientation for the robot to reach
   ROS_INFO("Sending dropoff location [%f. %f]", goal.target_pose.pose.position.x, goal.target_pose.pose.position.y);
   pose_pub.publish(goal);
-  ac.sendGoal(goal);
-  
-  ac.waitForResult();
-  // Check if the robot reached its goal
-  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED) {
-    ROS_INFO("Hooray, the base moved to dropoff location");
-    show.data = true;
-    pub.publish(show);
-  } else {
-    ROS_INFO("The base failed to move forward 1 meter for some reason");
-  }
-  ros::Duration(15.0).sleep();
+  show.data = true;
+  pub.publish(show);
+  ros::Duration(5.0).sleep();
+
   ros::spin();
   return 0;
 }
